656c1d6526b255b54dfeb385a113d14b57f80def
William Wong <willwong430@gmail.com>
2014-04-25 12:30:42 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Willywonga::Application.config.secret_key_base = '3f0d3195b969c6d160fd3214cd329f5cb4ee8dee46d6f19826988a4c2da7b31990a653eee6cac34fcb2bd53d69d431a9bf4f7fd59a6511770bcbb436ed2fec62'
